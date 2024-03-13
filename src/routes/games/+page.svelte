abd00ee983195fdad2ecbad7c7b93e5ca0738bb1
DavidFlisar <ddavidflisar@gmail.com>
2024-03-14 00:47:17 +0100

    Update badges in Svelte components

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -6,7 +6,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Play all your Favorite Games Online.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -17,7 +19,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Games available right at your fingertips.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -28,7 +32,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Steam games available as soon as they are updated.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -40,7 +46,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Frequently updated games.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -51,7 +59,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">One of the best repacks out there.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -62,7 +72,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Find your perfect game.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -73,7 +85,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Cracks made by one of the most trusted crackheads.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -84,7 +98,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Random Software and Games.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -96,7 +112,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Open Source Video Game Library.</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -107,7 +125,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Shop Cheap Keys for Games.</p>
-                <div class="badge badge-primary m-2">Windows</div><div class="badge badge-secondary m-2">Console</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div><div class="badge badge-secondary m-2">Console</div>
             </div>
         </a>
     </div>
--- src/routes/misc/+page.svelte
+++ src/routes/misc/+page.svelte
@@ -6,7 +6,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Online tools for making work easy.</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -17,7 +17,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Better Windows cursors</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -28,7 +30,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Old Context Menu for Windows 11</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -39,7 +43,9 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Remove annoying browsers from your life</p>
-                <div class="badge badge-primary m-2">Windows</div>
+                <div class="badge badge-primary m-2">
+                    <svg fill="oklch(var(--pc))" width="12px" height="12px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Windows XP icon</title><path d="M9.302 1.415c-1.184.02-2.592.32-4.341 1.044l-2.283 7.949c1.846-.761 3.311-1.114 4.537-1.1a7.596 7.596 0 014.37 1.593l2.296-7.92c-1.26-.855-2.607-1.599-4.58-1.566zm5.75 2.411l-2.256 7.949c2.016 1.367 4.44 2.494 8.907.493L24 4.333h-.042c-4.651 1.931-6.906.846-8.907-.507zM6.617 10.77c-1.184.018-2.591.315-4.335 1.034L0 19.779c4.65-1.93 6.863-.803 8.878.55l2.326-7.99c-1.26-.855-2.613-1.6-4.586-1.57zm5.784 2.344l.011.008.003-.008zm.011.008l-2.294 7.898c2.015 1.367 4.256 2.453 8.906.522l2.297-7.92c-4.641 1.927-6.882.85-8.909-.5Z"></path></g></svg>
+                </div>
             </div>
         </a>
     </div>
@@ -50,7 +56,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Check the colors on your screen.</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -72,7 +78,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Tool to check your PortForwarding</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -83,7 +89,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Install a lot of software in one click</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -94,7 +100,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Dummy data for programmers</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -105,7 +111,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Listen to Spotify without annoying ads</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -116,7 +122,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Random websites for boredom</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -127,7 +133,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Host your projects for free.</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -138,7 +144,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Check for available brand names on social media.</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -149,7 +155,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Tools for hackers and programmers.</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -157,11 +163,11 @@
         <a class="btn btn-accent card card-normal  h-32 m-3 indicator" href="https://www.svgrepo.com/" target="_blank">
             <span class="indicator-item badge badge-error">NEW</span> 
             <div>
-                <h1 class="font-bold text-2xl">SVG REPO</h1>
+                <h1 class="font-bold text-2xl">Svg Repo</h1>
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Open-licensed SVG Vector and Icons.</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
@@ -173,7 +179,7 @@
             </div>
             <div>
                 <p class="text-zinc-800 font-medium">Keyboard typing test.</p>
-                <div class="badge badge-primary m-2">Tools</div>
+                <div class="badge badge-success m-2">Tools</div>
             </div>
         </a>
     </div>
